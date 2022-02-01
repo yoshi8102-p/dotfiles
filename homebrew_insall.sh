a73a60269dbc8eb89d65d7182ca05ffc6abc59d9
Yoshiki Umeda <aiueo8102@gmail.com>
2022-02-01 12:56:43 +0900

    Homebrew install スクリプト追加

---
+++ homebrew_insall.sh
@@ -0,0 +1,16 @@
+echo "installing Homebrew ..."
+which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+cd $THIS_DIR
+
+echo "run brew doctor ..."
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "run brew update ..."
+which brew >/dev/null 2>&1 && brew update
+
+echo "ok. run brew upgrade ..."
+brew upgrade
+
+brew bundle
+
+brew cleanup
