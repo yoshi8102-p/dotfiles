ee09abe869966b5ce7eb55b9da5705e8f55a227f
Yoshiki Umeda <aiueo8102@gmail.com>
2022-02-03 19:01:11 +0900

    .hyper.js に gitrocket を追加

--- .hyper.js
+++ .hyper.js
@@ -145,7 +145,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyperpower", "hyperborder"],
+  plugins: ["hyperpower", "hyperborder", "gitrocket"],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
